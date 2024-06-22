237ca12ccd3263301b58d543164a1a89580630bc
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2024-06-22 11:48:56 +0530

    add

--- first-app/src/About.js
+++ first-app/src/About.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { createElement } from 'react';
 
 function About(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+    //jsx
+    // return (
+    //     <div>
+    //         <h1>About Us</h1>
+    //     </div>
+    // // );
+   // return React.createElement('div',null,'About Us')
+   return React.createElement('div',null,createElement('h1',null,'About Us'))
 }
 
 export default About;
