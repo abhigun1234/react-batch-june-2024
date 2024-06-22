b17f8c86acd877490ef5b3c7bbf306b4d243e626
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2024-06-22 12:09:15 +0530

    add

--- first-app/src/About.js
+++ first-app/src/About.js
@@ -1,14 +1,16 @@
 import React, { createElement } from 'react';
-
+import './About.css'
+import style from './appStyle.module.css'
 function About(props) {
     //jsx
-    // return (
-    //     <div>
-    //         <h1>About Us</h1>
-    //     </div>
-    // // );
+    return (
+        <div>
+            <h1 className='about'>errAbout Us</h1>
+            <h1 className={style.error}>About Us</h1>
+        </div>
+    );
    // return React.createElement('div',null,'About Us')
-   return React.createElement('div',null,createElement('h1',null,'About Us'))
+   //return React.createElement('div',null,createElement('h1',null,'About Us'))
 }
 
 export default About;
--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,15 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Greet';
 import Inline from './Inline';
-import Home from './Home';
-import About from './About';
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
-     <About></About>
-     </div>
+     <Greet name="raj"></Greet>
+     <Inline></Inline>
+     <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+    </div>
   );
 }
 
